662d338477809a3abb51e14876b714d4b492ad4e
AnshuKumar15 <goldenheavenlysunflower@gmail.com>
2026-04-06 03:01:20 +0530

    Add initial project configuration files and user schema definitions

---
+++ backend/app/utils/security.py
@@ -0,0 +1,55 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from app.config import settings
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app.models.user import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# -------------------------
+# PASSWORD FUNCTIONS
+# -------------------------
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# -------------------------
+# JWT TOKEN
+# -------------------------
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+# -------------------------
+# AUTH DEPENDENCY
+# -------------------------
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    db = SessionLocal()
+    user = db.query(User).filter(User.email == email).first()
+    db.close()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
